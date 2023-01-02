fbcd01f4a030cbee4e82c126e9d3d356beb8724f
Jakub Małdrzykowski <kubamaldrzyk@gmail.com>
2023-01-02 13:13:45 +0100

    Reformated files: utils.py

--- utils.py
+++ utils.py
@@ -1,13 +1,14 @@
 from enum import Enum
 
+
 class Directions(Enum):
     UP = "u"
     DOWN = "d"
     LEFT = "l"
     RIGHT = "r"
 
+
 class Distributions(Enum):
     NORMAL = 1
     UNIFORM = 2
     FIXED_VALUE = 3
-
